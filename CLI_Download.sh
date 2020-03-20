496cdbbda0ebd7a28b32221f6698b001ed0a4194
Isaac Zavala <isaac.zavala@ibm.com>
2020-03-20 15:51:30 -0500

    Updated linux packages

--- CLI_Download.sh
+++ CLI_Download.sh
@@ -5,7 +5,8 @@ download()
 {
     url="https://icp-console.mcmmah-105400-0143c5dd31acd8e030a1d6e0ab1380e3-0001.us-east.containers.appdomain.cloud:443/api/cli/"
     if [[ $1 == "1" ]];then 
-        
+        echo "$1"
+        echo "win"
         echo "downloading IBM Cloud Pak CLI" 
         curl -kLo cloudctl-win-amd64-v3.2.3-1557.exe "${url}cloudctl-${2}.exe"
         echo "downloading Kubernetes CLI" 
@@ -18,17 +19,18 @@ download()
         curl -kLo calicoctl-win-amd64-v3.5.2.exe "${url}calicoctl-${2}.exe"
 
     else
-        
+        echo "$1"
+        echo "non"
         echo "downloading IBM Cloud Pak CLI" 
-        curl -kLo cloudctl-win-amd64-v3.2.3-1557.exe "${url}cloudctl-${2}"
+        curl -kLo cloudctl-${2}-v3.2.3-1557 "${url}cloudctl-${2}"
         echo "downloading Kubernetes CLI" 
-        curl -kLo kubectl-win-amd64-v1.13.11.exe     "${url}kubectl-${2}"
+        curl -kLo kubectl-${2}-v1.13.11     "${url}kubectl-${2}"
         echo "downloading Helm CLI" 
-        curl -kLo helm-win-amd64-v2.12.3.tar.gz "${url}helm-${2}.tar.gz"
+        curl -kLo helm-${2}-v2.12.3.tar.gz "${url}helm-${2}.tar.gz"
         echo "downloading Istio CLI" 
-        curl -kLo istioctl-win-amd64-v1.2.2.exe "${url}istioctl-${2}"
+        curl -kLo istioctl-${2}-v1.2.2 "${url}istioctl-${2}"
         echo "downloading Calico CLI" 
-        curl -kLo calicoctl-win-amd64-v3.5.2.exe "${url}calicoctl-${2}"                 
+        curl -kLo calicoctl-${2}-v3.5.2 "${url}calicoctl-${2}"                 
     fi
 }
 
@@ -81,7 +83,8 @@ case "${unameOut}" in
         esac
            
     else
-        echo "Local Linux version not supported"  
+        echo "Local Linux version not supported" 
+        opt 
     fi
     ;;
     Darwin*)    
