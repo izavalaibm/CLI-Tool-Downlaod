8a6597d559deef993e09b6d0c8d71d00fac92900
Isaac Zavala <isaac.zavala@ibm.com>
2020-03-25 15:25:21 -0500

    Update commands to automatically download

--- CLI_Download.sh
+++ CLI_Download.sh
@@ -4,112 +4,88 @@
 download()
 {
     url="https://icp-console.mcmmah-105400-0143c5dd31acd8e030a1d6e0ab1380e3-0001.us-east.containers.appdomain.cloud:443/api/cli/"
+    ocUrl="https://mirror.openshift.com/pub/openshift-v4/clients/oc/latest"
+
     if [[ $1 == "1" ]];then 
         echo "downloading IBM Cloud Pak CLI" 
-        curl -kLo cloudctl-win-amd64-v3.2.3-1557.exe "${url}cloudctl-${2}.exe"
+        curl -kLo cloudctl.exe "${url}cloudctl-${2}.exe"
         echo "downloading Kubernetes CLI" 
-        curl -kLo kubectl-win-amd64-v1.13.11.exe     "${url}kubectl-${2}.exe"
+        curl -kLo kubectl.exe     "${url}kubectl-${2}.exe"
         echo "downloading Helm CLI" 
-        curl -kLo helm-win-amd64-v2.12.3.tar.gz "${url}helm-${2}.tar.gz"
+        curl -kLo helm.tar.gz "${url}helm-${2}.tar.gz"
         echo "downloading Istio CLI" 
-        curl -kLo istioctl-win-amd64-v1.2.2.exe "${url}istioctl-${2}.exe"
+        curl -kLo istioctl.exe "${url}istioctl-${2}.exe"
         echo "downloading Calico CLI" 
-        curl -kLo calicoctl-win-amd64-v3.5.2.exe "${url}calicoctl-${2}.exe"
+        curl -kLo calicoctl.exe "${url}calicoctl-${2}.exe"
+        echo "downloading oc CLI"
+        curl -kLo oc "${ocUrl}/windows/oc.zip"
 
     else
         echo "downloading IBM Cloud Pak CLI" 
-        curl -kLo cloudctl-${2}-v3.2.3-1557 "${url}cloudctl-${2}"
+        curl -kLo cloudctl "${url}cloudctl-${2}"
         echo "downloading Kubernetes CLI" 
-        curl -kLo kubectl-${2}-v1.13.11     "${url}kubectl-${2}"
+        curl -kLo kubectl     "${url}kubectl-${2}"
         echo "downloading Helm CLI" 
-        curl -kLo helm-${2}-v2.12.3.tar.gz "${url}helm-${2}.tar.gz"
+        curl -kLo helm.tar.gz "${url}helm-${2}.tar.gz"
+        tar -zxvf helm.tar.gz
         echo "downloading Istio CLI" 
-        curl -kLo istioctl-${2}-v1.2.2 "${url}istioctl-${2}"
+        curl -kLo istioctl "${url}istioctl-${2}"
         echo "downloading Calico CLI" 
-        curl -kLo calicoctl-${2}-v3.5.2 "${url}calicoctl-${2}"                 
+        curl -kLo calicoctl "${url}calicoctl-${2}" 
+        echo "downloading OpenShift Client CLI"
+        if [[ $1 == "3" ]];then
+            curl -kLo oc.tar.gz "${ocUrl}/linux/oc.tar.gz"
+            linux-amd64/helm
+            sudo mv linux-amd64/helm /usr/local/bin/helm
+            rm -rf linux-amd64
+        else 
+            curl -kLo oc.tar.gz "${ocUrl}/macosx/oc.tar.gz"
+            chmod 775 darwin-amd64
+            sudo mv darwin-amd64/helm /usr/local/bin/helm
+            rm -rf darwin-amd64
+            
+        fi
+        tar -zxvf oc.tar.gz
+        chmod 755 cloudctl oc kubectl istioctl calicoctl     
+        sudo mv oc /usr/local/bin/oc
+        sudo mv cloudctl /usr/local/bin/cloudctl   
+        sudo mv kubectl /usr/local/bin/kubectl
+        sudo mv istioctl /usr/local/bin/istioctl 
+        sudo mv calicoctl /usr/local/bin/calicoctl
+
+        ## Clean up 
+        rm helm.tar.gz	oc.tar.gz
     fi
 }
 
-opt()
-{
-    echo "What would you like to do?"
-    echo "1. Download for Mac"
-    echo "2. Download for Windows(64-bit)"
-    echo "3. Download for Linux (64-bit)"
-    echo "4. Download for Linux(ppc64le)"
-    echo "5. Download for Linux(s390x)"
-    echo "6. Exit without download"
-    read -p "Please select a number: " yn
-            case $yn in
-            [1] ) 
-                echo "Downloading CLI for Mac"
-                download 2 darwin-amd64 exit ;;
-            [2] ) 
-                echo "Downloading CLI for Windows(64-bit)"
-                download 1 win-amd64 exit ;;
-            [3] )
-                echo "Downloading CLI for Linux (64-bit)" 
-                download 2 linux-amd64 exit ;;   
-            [4] ) 
-                echo "Downloading CLI for Linux(ppc64le)"
-                download 2 linux-ppc64le exit ;; 
-            [5] )
-                echo "Downloading CLI for Linux(s390x)" 
-                download 2 linux-s390x exit ;;
-            [6] ) 
-                echo "Exiting without downloading" exit ;;
-            * ) echo "Please choose a number from the list" exit ;;
-            esac
-}
 
 ## Gets OS, if OS is supported it will download the needed CLI. If not suported or user wants to download a specific 
-## OS they can download it by selecting which they want to
+## OS they can download it by selecting which OS they want to dowload the CLI for
 unameOut="$(uname -s)"
 case "${unameOut}" in
     Linux*)
     linV="$(uname -p)"
-    if [[ $linV == *"x86_64"* ]];then 
-        read -p "Do you wish to install CLI for Linux(64-bit)? " yn
-        case $yn in
-        [Yy]* ) 
-            download 2 linux-amd64 exit ;;
-        [Nn]* ) 
-            opt ;;
-        * ) echo "Please answer yes or no."  exit ;;
-        esac
-           
+    if [[ $linV == *"x86_64"* ]];then
+        echo "Installing CLI for Linux"  
+        download 3 linux-amd64 exit 
     else
         echo "Local Linux version not supported" 
-        opt 
     fi
     ;;
     Darwin*)    
-    read -p "Do you wish to install CLI for Mac?" yn
-        case $yn in
-        [Yy]* ) 
-            download 2 darwin-amd64 exit ;;
-        [Nn]* ) 
-            opt ;;
-        * ) echo "Please answer yes or no." exit;;
-        esac
+        echo "Installing CLI for Mac" 
+        download 2 darwin-amd64 exit 
     ;;
     MSYS_NT*)  
     minV="$(uname -m)"   
     if [[ $minV == *"x86_64"* ]];then 
-        read -p "Do you wish to install CLI for Windows(64-bit)?" yn
-            case $yn in
-            [Yy]* ) 
-                download 1 win-amd64 exit ;;
-            [Nn]* ) 
-                opt ;;
-            * ) echo "Please answer yes or no." exit;;
-            esac
+        echo "Installing CLI for Windows(64-bit)?" 
+        download 1 win-amd64 exit 
     else
         echo "Local Microsoft version not supported" 
-        opt 
     fi
     ;;
     *)          
         echo "Local OS not recognized or unsuported"
-        opt;;
+    ;;
 esac
